1f30d7eddc72f9a19db38990d79411e5c74a08a8
x-Shen <shenx3@uci.edu>
2015-09-22 10:26:17 -0700

    present stories

--- stream_of_stories.py
+++ stream_of_stories.py
@@ -4,11 +4,16 @@ Created on Sep 20, 2015
 @author: xinshen
 '''
 import tool_box
+
 # Generate and Present Ten Stories
 
 def generate_ten_stories():
-    pass
-
+    stories = tool_box.get_top_n_stories_id(tool_box.base_url, 10, tool_box.context)
+    story_list = []
+    for i in range(10):
+        story_list.append(tool_box.get_story_by_id(stories[i]))
+    return story_list
+        
 
 def present_ten_stories():
     pass
--- tool_box.py
+++ tool_box.py
@@ -19,12 +19,12 @@ def get_json(base,item,context):
 def convert_to_str(b_str):
     return b_str.decode("utf-8")
 
-def get_top_n_stories_id(base,n,context):
-    ids = get_json(base,'topstories',context)
+def get_top_n_stories_id(base,n,type_of,context):
+    ids = get_json(base,type_of,context)
     id_str = convert_to_str(ids)[1:-1].split(',')
     return id_str[:n]
 
-l = get_top_n_stories_id(base_url,10,context)
+#l = get_top_n_stories_id(base_url,10,context)
 
 
 def get_story_by_id(base,ID,context):
@@ -39,8 +39,26 @@ def present_story(d):
     s = d['title']+'\t'+str(d['score'])+'\n'+d['url']+'\n'
     return s
 
-j = get_story_by_id(base_url,l[0],context)
-print(present_story(json_to_dict(j)))
+#j = get_story_by_id(base_url,l[0],context)
+#print(present_story(json_to_dict(j)))
+
+def generate_ten_stories(base,context,type_of):
+    stories = get_top_n_stories_id(base, 10,type_of, context)
+    story_list = []
+    for i in range(10):
+        story_list.append(get_story_by_id(base,stories[i],context))
+    return story_list
+
+def present_story_list(stories):
+    for story in stories:
+        print(present_story(json_to_dict(story)))
+
+top_stories = generate_ten_stories(base_url,context,'topstories')
+new_stories =  generate_ten_stories(base_url,context,'newstories')
+
+
+    
+
 
 
 
